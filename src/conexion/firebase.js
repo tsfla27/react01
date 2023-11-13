0949107d6993e9f78faa83296cc6d161910260c4
tito <tsfla82@gmail.com>
2023-11-12 21:16:31 -0500

    version 4.0 se completo el crud

---
+++ src/conexion/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB60cUOn39RrZmP670NXB8mDRlcvaEgr9Y",
+  authDomain: "react-app1-698be.firebaseapp.com",
+  databaseURL: "https://react-app1-698be-default-rtdb.firebaseio.com",
+  projectId: "react-app1-698be",
+  storageBucket: "react-app1-698be.appspot.com",
+  messagingSenderId: "665015838062",
+  appId: "1:665015838062:web:b007cfa93ba7c9a638cd7f",
+  measurementId: "G-8YMZQGKF4Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
--- src/protegido/Applista.js
+++ src/protegido/Applista.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppForm from './AppForm'
-
+import { collection, deleteDoc, doc, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../conexion/firebase';
 const Applista = (props) => {
+  
+  //////// lectura fnRead
+  const [docBD, setDocDB] = useState([]);
+  const fnRead = () => {
+    const xColeccionConQuery = query(collection(db, 'persona'));
+    const unsubcribe = onSnapshot(xColeccionConQuery,(xDatosDB)=> {
+      const xDoc = [];
+      xDatosDB.forEach((doc) => {
+        xDoc.push({id:doc.id, ...doc.data()});
+      });
+      setDocDB(xDoc);
+    })
+  }
+  //fnRead();
+  useEffect(()=>{fnRead();}, [props.idActual]);
+//console.log (docBD)
+  /////// delete
+  const [idActual, setIdActual] = useState("");
+  const fnDelete = async (xId) => {
+    if (window.confirm("Confirme para eliminar")){
+      await deleteDoc(doc(db,"persona", xId));
+    }
+    alert("Se elimino con exito...");
+  }
   return (
     <div style={{background:"greenyellow", padding:"10px"}}>
         <h1>Applist.js</h1>
+        <AppForm {...{idActual, setIdActual}}/>
         <h3>Lista de Clientes</h3>
-        <AppForm/>
-        <p>No. 1. Juan Manuel Ticona Vega ..... x ..... A</p>
-        <p>No. 2. Rosa Maria Luque Conde  ..... x ..... A</p>
-        <p>No. 3. Joe Ricardo Lopez Moral ..... x ..... A</p>
+        {
+          docBD.map((row, index) =>
+          <p key={row.id}>
+            No. {index + 1}. {row.nombre}
+            .....
+            <span onClick={()=> fnDelete(row.id)}>x</span>
+            .....
+            <span onClick={()=> setIdActual(row.id)}>A</span>
+          </p> )
+        }
+        
+        
         </div>
   )
 }
